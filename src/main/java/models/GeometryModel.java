458c3f7111495fe7e9ab94e66bf33872057fd455
ddanaj <david.danaj@gmail.com>
2015-10-02 13:06:59 +0200

    frigg fixes2

--- src/main/java/models/GeometryModel.java
+++ src/main/java/models/GeometryModel.java
@@ -1,7 +1,5 @@
 package models;
 
-
-import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
