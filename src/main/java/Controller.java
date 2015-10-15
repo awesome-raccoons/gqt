bf346078d6d51831b9e6eddcce4772eb97cd4c2f
Johannes Moskvil <johannesmos@hotmail.com>
2015-10-15 14:41:05 +0200

    Added scaleable grid

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,9 +1,12 @@
 import javafx.fxml.FXML;
 import javafx.scene.Cursor;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
@@ -36,6 +39,8 @@ public class Controller {
     private double mouseMoveOffsetX = 0;
     private double mouseMoveOffsetY = 0;
 
+    private BackgroundGrid backgroundGrid;
+
     @FXML
     private TextArea queryInput;
     @FXML
@@ -62,6 +67,10 @@ public class Controller {
 
     public Controller() {
 
+    }
+
+    public AnchorPane getUpperPane() {
+        return upperPane;
     }
 
 
@@ -228,6 +237,10 @@ public class Controller {
 
     private void setZoomLevel() {
         currentZoom = getZoomScale(ZOOM_FACTOR, currentZoomLevel);
+        //The zoomed changed, resize the grid
+        //System.out.println("zoom changed");
+        //System.out.println((int)currentZoom);
+        backgroundGrid.scaleGrid((int)(currentZoom * 10), (int)(currentZoom * 10));
     }
     public final void resetView() {
         currentZoomLevel = 0;
@@ -315,6 +328,10 @@ public class Controller {
      * Called when mouse releases on upperPane. Makes sure cursor goes back to normal.
      */
     public final void upperPaneMouseReleased(final MouseEvent event) {
+        if (event.getButton() == MouseButton.SECONDARY) {
+            backgroundGrid.showContextMenu(event.getScreenX(), event.getScreenY());
+        }
+
         this.stage.getScene().setCursor(Cursor.DEFAULT);
         this.currentOffsetX += event.getSceneX() - dragBeginX;
         this.currentOffsetY += event.getSceneY() - dragBeginY;
@@ -384,4 +401,9 @@ public class Controller {
     public static boolean isKeyHeldDown(final KeyCode code) {
         return heldDownKeys.contains(code);
     }
+
+    public final void setBackgroundGrid(BackgroundGrid bg) {
+        this.backgroundGrid = bg;
+    }
+
 }
